4fdbad7f74f75126c0aec226e55f9d0d6c95fc26
repl.it user <replituser@example.com>
2021-03-02 10:11:24 +0000

    Demos

---
+++ Demos/if.py
@@ -0,0 +1,6 @@
+print("What is your name?")
+n = input()
+if len(n)>9:
+  print("You have a realy long name!")
+
+print("program end")
--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("Has to be something in here probably")
+print("What is your name?")
+n = input()
+if len(n)>9:
+  print("You have a realy long name!")
+
+print("program end")
