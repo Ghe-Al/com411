1599f9fe9e13287e7b0d017e0ea52df51cc7ca1e
repl.it user <replituser@example.com>
2021-03-05 10:37:16 +0000

    added if demonstration

---
+++ basics/decisions/simple_decision/if.py
@@ -0,0 +1,5 @@
+print("What type of book is this?")
+b=str(input())
+if b=="adventure" or b=="Adventure":
+  print("I like adventure books!")
+print("Finished reading book!")
--- main.py
+++ main.py
@@ -1,6 +1,27 @@
-print("What is your name?")
-n = input()
-if len(n)>9:
-  print("You have a realy long name!")
+print("Please choose an option from the menu: \n1 - Nice message \n2 - Area of a rectangle \n3 - Area of a triangle \n4 - Times table")
 
-print("program end")
+option = int(input())
+
+if option == 1:
+  print("Today will be a good day!")
+elif option == 2:
+  print("Enter the length of the rectangle:")
+  l=int(input())
+  print("Enter the width of the rectangle:")
+  w=int(input())
+  area=w*l
+  print("The area of this rectangle is {}".format(area))
+elif option == 3:
+  print("Enter the length of a base:")
+  l=float(input())
+  print("Enter the length of the corresponding height:")
+  h=float(input())
+  area = 0.5*l*h
+  print("The area of the triangle is {}".format(area))
+elif option == 4:
+  print("What number would you like to see times table for?")
+  n = int(input())
+  for i in range(1,11,1):
+    print("{}x{}={}".format(n,i,n*i))
+else:
+  print("Invalid choice")
