3da99e1861027529accea10cf3e06b2e41e2eae3
repl.it user <replituser@example.com>
2021-03-05 11:28:27 +0000

    Added comparison operator demonstration

---
+++ basics/decisions/simple_decision/comparison_operators.py
@@ -0,0 +1,10 @@
+print("Enter your first number:")
+n=int(input())
+print("Enter your second number:")
+m=int(input())
+if n>m:
+  print("The first number is higher!")
+elif n<m:
+  print("The second number is higher!")
+else:
+  print("The numbers are equal!")
